62de9e523a3c083b5e445210294d9b0b20fb9766
Dasha <d.gazkaeva@gmail.com>
2025-02-08 13:32:02 +0300

    delete prints

--- cut/cut.py
+++ cut/cut.py
@@ -9,7 +9,6 @@ UNKNOWN_NAME = 'unknown'
 
 
 def handler(event, context):
-    print(event)
     for message in event['messages']:
         body = message['details']['message']['body']
         message = json.loads(body)
@@ -22,7 +21,6 @@ def handler(event, context):
         output_path = f'{PATH_FACES}/{new_object_id}'
 
         load_crop_and_save(image_path, output_path, message['coordinates'])
-        print(f'Saved to {output_path}')
 
     return {'statusCode': 200, 'body': ''}
 
@@ -31,18 +29,14 @@ def load_crop_and_save(image_path, output_path, coordinates):
     x, y, x1, y1 = coordinates
     img = cv2.imread(image_path)
 
-    # Check if the image was loaded successfully
     if img is None:
       print(f'Error: Could not open or find the image at {image_path}')
       return
-
-    # Crop the image using array slicing
+    
     cropped_img = img[y:y1, x:x1]
 
-    # Check if cropping resulted in an empty image
     if cropped_img.size == 0:
       print('Error: Cropped image has zero size. Check your coordinates.')
       return
-
-    # Save the cropped image
+    
     cv2.imwrite(output_path, cropped_img)
